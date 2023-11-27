c1453fffd5e48527409e6a429133115f47473ab7
Ailton Moreira <amv265@hotmail.com>
2023-11-27 12:11:15 +0000

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <title>IMDb Clone</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="This is the IMDb clone" />
+          <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
