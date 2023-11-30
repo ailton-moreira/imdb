5c5ef806015a390a408c790b88510001eecf1aec
Ailton Moreira <amv265@hotmail.com>
2023-11-30 16:49:35 +0000

    add dark mode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Link from 'next/link'
-import MenuItem from "./MenuItem";
+import MenuItem from "@/app/components/MenuItem";
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
+import DarkModeSwitch from "@/app/components/DarkModeSwitch";
 
 export default function Header() {
   return (
@@ -11,7 +12,8 @@ export default function Header() {
         <MenuItem title={"Home"} address={"/"} Icon={AiFillHome}/>
         <MenuItem title={"About"} address={"/about"} Icon={BsFillInfoCircleFill}/>
       </div>
-          <div className="">
+      <div className="flex flex-row items-center space-x-5">
+        <DarkModeSwitch />
               <Link href="/">
                   <h2 className="text-2xl"><span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span><span className="text-xl hidden sm:inline">Clone</span></h2>
               </Link>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,29 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from './components/Header'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/app/components/Header";
+import Providers from "@/app/Providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'IMDb Clone App',
-  description: 'This is the IMDb clone',
-}
+  title: "IMDb Clone App",
+  description: "This is the IMDb clone",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-
-        {/**HEADER */}
+        <Providers>
+          {/**HEADER */}
           <Header />
-        {/**NAVBAR */}
+          {/**NAVBAR */}
 
-        {/**SearchBar */}
-        
-        {children}</body>
+          {/**SearchBar */}
+
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
