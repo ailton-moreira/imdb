845f04aa7e7177f2d25315d243542a2648a9cb08
Ailton Moreira <amv265@hotmail.com>
2023-12-01 16:55:05 +0000

    added fetch data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,36 @@
-import Image from 'next/image'
+import axios from "axios";
+import Results from "@/app/components/Results";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  /* const { data } = await axios({
+    method: 'GET',
+    url: `${process.env.BASE_URL}/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${process.env.API_KEY}&language=en-US&page=1`,
+    headers: {
+      'Content-Type': 'application/json', 
+    },
+  })*/
+
+  const res = await fetch(
+    `${process.env.BASE_URL}/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${process.env.API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data!')
+  }
+
+  const { results } = await res.json();
+  
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
-      
-    </main>
-  )
+    <div className="flex">
+      <Results results={results}/>
+    </div>
+  );
 }
